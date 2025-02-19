71823953439c792c67de3ea3968310f18aa9f912
SamuelMoores <samueldmoores@gmail.com>
2025-02-18 20:31:53 -0800

    Releasus of Suburbia

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     Animator animator;
+    float conjurTimer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        animator = GetComponent<Animator>();    
+        animator = GetComponent<Animator>();
+        conjurTimer = 2.0f;
     }
 
     // Update is called once per frame
@@ -16,10 +18,27 @@ public class PlayerAttack : MonoBehaviour
         if(Input.GetKey(KeyCode.Space))
         {
             animator.SetBool("conjur", true);
+            conjurTimer -= Time.deltaTime;
         }
         else
         {
             animator.SetBool("conjur", false);
         }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if(conjurTimer <= 0.0f)
+            {
+                animator.SetBool("release", true);
+            }
+
+                conjurTimer = 2.0f;
+        }
+       
+    }
+
+    public void ReleaseReset()
+    {
+        animator.SetBool("release", false);
     }
 }
